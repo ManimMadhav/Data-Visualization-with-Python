c9dd31653f11035591765208c31e737cd3787b1f
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-21 11:33:47 +0530

    Update part2.py

--- studentGradePrediction/part2.py
+++ studentGradePrediction/part2.py
@@ -1,1 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+#comparing based on number of failures and internet access
+dataset = pd.read_csv("student-mat.csv")
+failures = dataset['failures']
+internetAccess = dataset['internet']
+
+internetAccessWith0Fails = []
+for i in range(0,len(failures)):
+    if failures[i]==0:
+        ch = internetAccess[i]
+        internetAccessWith0Fails.append(ch)
+
+#returns the array with students who have 0 failures 
+#and yes/no on whether they have internet access or not
+print(internetAccessWith0Fails)
+
+noOfYes = 0
+noOfNo = 0
+
+Fails0Array = [] 
+for i in internetAccessWith0Fails:
+    if i == 'yes':
+        noOfYes += 1
+    else:
+        noOfNo += 1
+
+print(noOfNo)
+print(noOfYes)
+lst=[]
+lst.append(noOfNo)
+lst.append(noOfYes)
+
+plt.pie(lst,autopct="%.2f",labels=["Don't Have\n internet access","Have\n internet access"])
+plt.show()
