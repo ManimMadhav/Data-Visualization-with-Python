92aecfdaf9c2cfaeaab2d43906774a2bb68de866
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-21 11:33:28 +0530

    Update part1.py

--- studentGradePrediction/part1.py
+++ studentGradePrediction/part1.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+#absences vs final grade (comparative)
+
+dataset = pd.read_csv(r"student-mat.csv")
+absences = dataset['absences']
+finalgrade = dataset['G3']
+
+
+plt.bar(absences,finalgrade,color='red',width=0.7)
+plt.xlabel("Absences")
+plt.ylabel("Marks")
+plt.title("Absences vs Marks Bar Graph for Students")
+plt.show()
